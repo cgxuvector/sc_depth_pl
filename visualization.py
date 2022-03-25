d6d5334ed2764b7826758982e733aa6081b513eb
cgxuvector <xu.cheng@husky.neu.edu>
2022-03-25 11:56:24 -0400

    remote-panzer-change

--- visualization.py
+++ visualization.py
@@ -17,11 +17,11 @@ def visualize_depth(depth, cmap=cv2.COLORMAP_JET):
     """
     depth: (H, W)
     """
+    cmap = cv2.COLORMAP_BONE # set the color map to be depth colormap
     x = depth.cpu().numpy()
     x = np.nan_to_num(x)  # change nan to 0
     mi = np.min(x)  # get minimum depth
     ma = np.max(x)
-    print(mi, ma)
     x = (x-mi)/(ma-mi+1e-8)  # normalize to 0~1
     x = (255*x).astype(np.uint8)
     x_ = Image.fromarray(cv2.applyColorMap(x, cmap))
